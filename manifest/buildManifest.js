839146bbbd3ae87c4a66efa762f686ca1a0f088e
anbuhckr <36891836+anbuhckr@users.noreply.github.com>
2024-06-29 16:00:30 +0700

    Update buildManifest.js

--- manifest/buildManifest.js
+++ manifest/buildManifest.js
@@ -5,6 +5,6 @@ self.__BUILD_MANIFEST = {
         fallback: []
     },
     "/": ["chunks/542b50fd-07ebedc579cef971.js", "chunks/617-c003b39bbc271299.js", "chunks/pages/index-5cf0f95e4ab7f3cf.js"],
-    "/_error": ["chunks/pages/_error-8447282b6bcee29e.js"],
-    sortedPages: ["/", "/_app", "/_error"]
+    "/error": ["chunks/pages/error-8447282b6bcee29e.js"],
+    sortedPages: ["/", "/app", "/error"]
 }, self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
