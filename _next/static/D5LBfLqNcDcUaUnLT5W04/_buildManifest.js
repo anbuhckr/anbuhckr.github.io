bf8edbbd8fd785baee51f2c339e8b1fc3c6f4b3f
anbuhckr <36891836+anbuhckr@users.noreply.github.com>
2024-06-29 12:20:50 +0700

    Delete _next/static directory

--- _next/static/D5LBfLqNcDcUaUnLT5W04/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/542b50fd-07ebedc579cef971.js","static/chunks/617-c003b39bbc271299.js","static/chunks/pages/index-5cf0f95e4ab7f3cf.js"],"/_error":["static/chunks/pages/_error-8447282b6bcee29e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
