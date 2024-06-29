0c5864d779871dfa23790cd80211314a457fe788
anbuhckr <36891836+anbuhckr@users.noreply.github.com>
2024-06-29 12:30:25 +0700

    Create _buildManifest.js

---
+++ manifest/_buildManifest.js
@@ -0,0 +1,10 @@
+self.__BUILD_MANIFEST = {
+    __rewrites: {
+        afterFiles: [],
+        beforeFiles: [],
+        fallback: []
+    },
+    "/": ["chunks/542b50fd-07ebedc579cef971.js", "chunks/617-c003b39bbc271299.js", "chunks/pages/index-5cf0f95e4ab7f3cf.js"],
+    "/_error": ["chunks/pages/_error-8447282b6bcee29e.js"],
+    sortedPages: ["/", "/_app", "/_error"]
+}, self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
